f6e344cc20a00fd6699fc654fd15a641f084cb0d
wildpow <element1028@gmail.com>
2018-12-16 15:40:59 -0800

    first two tests for roverFinder

--- cypress/integration/test_spec.js
+++
@@ -1,7 +0,0 @@
-import roverFinder from '../../src/roverFinder';
-
-describe('isPalindrome', () => {
-  it('returns true if the string is a palindrome', () => {
-    expect(roverFinder('abba')).toEqual(true);
-  });
-});
